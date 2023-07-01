1243d7d2ed98278563ae5ea9cda6ceaad64217ba
Lucas Zunino <zunino10@hotmail.com>
2023-06-30 21:10:32 -0300

    req4 half-done!

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express, { Request, Response } from 'express';
 import productsRouter from './routers/products.router';
 import ordersRouter from './routers/orders.router';
+import loginRouter from './routers/login.router';
 
 const app = express();
 
 app.use(express.json());
+app.use(loginRouter);
 app.use(productsRouter);
 app.use(ordersRouter);
 
---
+++ src/services/login.service.ts
@@ -0,0 +1,30 @@
+import { ServiceResponse } from '../types/ServiceResponse';
+import { Login } from '../types/Login';
+import { Token } from '../types/Token';
+import UserModel from '../database/models/user.model';
+import jwtUtil from '../utils/jwt.util';
+
+async function login(payload: Login): Promise<ServiceResponse<Token>> {
+  if (!payload.username || !payload.password) {
+    return { status: 'INVALID_DATA', data: { message: '"username" and "password" are required' } };
+  }
+
+  const foundUser = await UserModel.findOne({ where: { username: payload.username } });
+
+  if (!foundUser || foundUser.dataValues.password !== payload.password) {
+    return {
+      status: 'UNAUTHORIZED',
+      data: { message: 'Username or password invalid' },
+    };
+  }
+
+  const { id, username } = foundUser.dataValues;
+
+  const token = jwtUtil.sign({ id, username });
+
+  return { status: null, data: { token } };
+}
+
+export default {
+  login,
+};
