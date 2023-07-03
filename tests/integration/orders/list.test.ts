16240d9bf39db47e186833f093210958d9ee046b
Lucas Zunino <zunino10@hotmail.com>
2023-07-02 22:39:21 -0300

    req3 done!

--- tests/integration/orders/list.test.ts
+++ tests/integration/orders/list.test.ts
@@ -1,10 +1,31 @@
-import sinon from 'sinon';
-import chai, { expect } from 'chai';
-import chaiHttp from 'chai-http';
+import sinon from "sinon";
+import chai, { expect } from "chai";
+import chaiHttp from "chai-http";
+
+import OrderModel, {
+  OrderSequelizeModel,
+} from "../../../src/database/models/order.model";
+import app from "../../../src/app";
+import ordersMock from "../../mocks/orders.mock";
 
 chai.use(chaiHttp);
 
-describe('GET /orders', function () { 
-  beforeEach(function () { sinon.restore(); });
+describe("GET /orders", function () {
+  beforeEach(function () {
+    sinon.restore();
+  });
 
+  describe("listagem de ordens", function () {
+    it("findAll", async function () {
+      const ordList = ordersMock.orderList.map((o) =>
+      OrderModel.build(o)
+      );
+      sinon.stub(OrderModel, "findAll").resolves(ordList);
+
+      const { status, body } = await chai.request(app).get("/orders");
+
+      expect(status).to.equal(200);
+      expect(body).to.deep.equal(ordersMock.orderList);
+    });
+  });
 });
---
+++ tests/mocks/orders.mock.ts
@@ -0,0 +1,18 @@
+const orderList = [
+	{
+		id: 1,
+		userId: 1
+	},
+	{
+		id: 2,
+		userId: 3
+	},
+	{
+		id: 3,
+		userId: 2
+	}
+]
+
+export default {
+  orderList,
+}
