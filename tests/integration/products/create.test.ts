e192400df90dce2161e6705b8e479924c4242fb5
Lucas Zunino <zunino10@hotmail.com>
2023-07-02 21:13:00 -0300

    req1 done!

--- tests/integration/products/create.test.ts
+++ tests/integration/products/create.test.ts
@@ -1,10 +1,28 @@
 import sinon from 'sinon';
 import chai, { expect } from 'chai';
 import chaiHttp from 'chai-http';
+import productsMock from '../../mocks/products.mock';
+// import productsMock from '../../mocks/products.mock';
+import productsService from '../../../src/services/products.service';
+import app from '../../../src/app';
 
 chai.use(chaiHttp);
 
-describe('POST /products', function () { 
+describe('POST /products', function () {
   beforeEach(function () { sinon.restore(); });
 
+  describe('manda input valido', function () {
+    it('retorna produto com id', async function () {
+      const requestBody = productsMock.validInput;
+      sinon.stub(productsService, 'registerProduct').resolves({ status: null, data: productsMock.validOutput })
+
+      const response = await chai.request(app).post('/products').send(requestBody);
+
+      expect(response.status).to.equal(201);
+      expect(response.body).to.deep.equal(productsMock.validOutput);
+    })
+  })
+ 
+
+  
 });
---
+++ tests/mocks/products.mock.ts
@@ -0,0 +1,16 @@
+const validInput = {
+  name: "Martelo de Thor",
+  price: "30 peças de ouro",
+  orderId: 4,
+};
+
+const validOutput = {
+  id: 6,
+  name: "Martelo de Thor",
+  price: "30 peças de ouro",
+};
+
+export default {
+  validInput,
+  validOutput,
+};
