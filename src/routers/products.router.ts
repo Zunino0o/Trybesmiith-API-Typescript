c3a511f039f1efdf8dea09012576a780a9c1cedf
Lucas Zunino <zunino10@hotmail.com>
2023-07-02 23:58:04 -0300

    req5 done!

--- src/routers/products.router.ts
+++ src/routers/products.router.ts
@@ -1,9 +1,15 @@
 import { Router } from 'express';
 import productsController from '../controllers/products.controller';
+import auth from '../middlewares/product.validations';
 
 const productsRouter = Router();
 
 productsRouter.get('/products', productsController.listProducts);
-productsRouter.post('/products', productsController.registerProduct);
+productsRouter.post(
+  '/products',
+  auth.validateName,
+  auth.validatePrice,
+  productsController.registerProduct,
+);
 
 export default productsRouter;
